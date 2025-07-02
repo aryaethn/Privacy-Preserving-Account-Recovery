341ce33c605ac4def715c26439ae557b23cb3fc9
AriaNaraghi <arianari97@gmail.com>
2025-07-02 10:22:55 +0300

    Project updates: restructure, add new circuits, update Go and JS, remove old files, update README.md

--- PPAR.bash
+++
@@ -1,4 +0,0 @@
-circom ppar_google_pw.circom --r1cs --wasm -o build
-# universal CRS assumed (PLONK on BLS12-381)
-snarkjs plonk setup build/ppar_google_pw.r1cs powersOfTau28_hez_final_20.ptau ppar_pw.zkey
-snarkjs zkey export solidityverifier ppar_pw.zkey VerifierGooglePw.sol
